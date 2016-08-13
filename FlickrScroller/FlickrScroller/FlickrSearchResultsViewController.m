cd24d34419888c044451e69e4e0416a9fb358c8d
Dhiraj Gupta <dhiraj@traversient.com>
2016-08-13 13:07:20 +0530

    Added empty seach results controller, with custom designated initializer method, marking others as unavailable.

---
+++ FlickrScroller/FlickrScroller/FlickrSearchResultsViewController.m
@@ -0,0 +1,16 @@
+//
+//  FlickrSearchResultsViewController.m
+//  FlickrScroller
+//
+//  Created by Dhiraj Gupta on 8/13/16.
+//  Copyright © 2016 Traversient Tech LLP. All rights reserved.
+//
+
+#import "FlickrSearchResultsViewController.h"
+
+@implementation FlickrSearchResultsViewController
+- (nullable instancetype) initWithSearchPhrase:(nonnull NSString *) searchPhrase {
+    self = [super initWithNibName:nil bundle:nil];
+    return self;
+}
+@end
