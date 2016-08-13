9f5fc368e23bf0cf63f24dd9d7eba6e382df31e3
Dhiraj Gupta <dhiraj@traversient.com>
2016-08-13 12:18:43 +0530

    Added search bar, search button and basic fleshing out of performing search.

--- FlickrScroller/FlickrScroller/SearchViewController.m
+++ FlickrScroller/FlickrScroller/SearchViewController.m
@@ -7,7 +7,50 @@
 //
 
 #import "SearchViewController.h"
+#import "AppDelegate.h"
 
+@interface SearchViewController ()<UISearchBarDelegate>
+@property (nonatomic,strong) UITableView * tableView;
+@property (nonatomic,strong) NSMutableArray <NSString *>* arrPastSearches;
+@property (nonatomic,strong) UIBarButtonItem * barbuttonSearch;
+@property (nonatomic,strong) UISearchBar * searchBar;
+@end
 @implementation SearchViewController
+#pragma mark - Private
+- (void) performSearchWithPhrase:(NSString *)searchPhrase{
+    DLog(@"Begin search with phrase:%@",searchPhrase);
+    [self.searchBar resignFirstResponder];
+    //Add into searches array
+    //Load results view controller
+}
+#pragma mark - Actions
+- (void) navbarSearchButtonTapped:(id)sender{
+    [self performSearchWithPhrase:self.searchBar.text];
+}
 
+#pragma mark - LifeCycle
+- (void) viewDidLoad{
+    self.title = S_SearchFlickr;
+    self.arrPastSearches = [NSMutableArray array];
+    self.barbuttonSearch = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSearch target:self action:@selector(navbarSearchButtonTapped:)];
+    self.barbuttonSearch.enabled = NO;
+    self.navigationItem.rightBarButtonItem = self.barbuttonSearch;
+    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectZero];
+    self.searchBar.placeholder = S_SearchFlickr;
+    self.searchBar.delegate = self;
+    self.navigationItem.titleView = self.searchBar;
+}
+
+#pragma mark - UISearchBar
+- (void) searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
+    if ([BUtil isValidString:searchText]) {
+        self.barbuttonSearch.enabled = YES;
+    }
+    else{
+        self.barbuttonSearch.enabled = NO;
+    }
+}
+- (void) searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+    [self performSearchWithPhrase:searchBar.text];
+}
 @end
