6a1c3bddcf21e306316022e1a0eab144e24cdaeb
Dhiraj Gupta <dhiraj@traversient.com>
2016-08-14 13:36:24 +0530

    Removed unused util methods.

--- FlickrScroller/FlickrScroller/Utils/BUtil.m
+++ FlickrScroller/FlickrScroller/Utils/BUtil.m
@@ -7,7 +7,6 @@
 //
 
 #import "BUtil.h"
-//#define USE_TEST_COLORS 1
 
 @implementation BUtil
 + (BOOL) isValidArray: ( id ) object
@@ -69,45 +68,13 @@
     return NO;
 }
 
-+ ( BOOL ) isValidNumber:( id ) object
-{
-    if ( object != nil &&
-        [NSNull null] != object &&
-        [object isKindOfClass:[NSNumber class]] )
-    {
-        return YES;
-    }
-    
-    return NO;
-}
 + (BOOL) isValidDataObject:(id)object{
     if (object != nil && [NSNull null] != object && [object isKindOfClass:[NSData class]]) {
         return [object length] > 0;
     }
     return NO;
 }
-+ (CGFloat) halfOf:(CGFloat)half{
-    return floorf(half* 0.5f);
-}
-+ (CGFloat) oneThirdOf:(CGFloat)full{
-    return floorf(full* 0.33333333f);
-}
-+ (CGFloat) twoThirdsOf:(CGFloat)full{
-    return floorf(full* 0.66666666f);
-}
-+ (CGFloat) oneFourthOf:(CGFloat)full{
-    return floorf(full* 0.25f);
-}
-+ (CGFloat) oneFifthOf:(CGFloat)full{
-    return floorf(full* 0.20f);
-}
 + (UINavigationController *)navigationControllerWithRoot:(UIViewController *)controller{
     return [[UINavigationController alloc] initWithRootViewController:controller];
-}
-+ (BOOL) string:(NSString *)stringA isSameAs:(NSString *)stringB{
-    return [stringA compare:stringB options:NSCaseInsensitiveSearch] == NSOrderedSame;
-}
-+ (BOOL) urlRequest:(NSURLRequest *)request1 hasSameURLAs:(NSURLRequest *)request2{
-    return [request1.URL.absoluteString compare:request2.URL.absoluteString options:NSCaseInsensitiveSearch] == NSOrderedSame;
 }
 @end
